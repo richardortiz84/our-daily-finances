6d078dccd8d7901a95d80b76c0dc29e9d9a35f21
Richard Ortiz <richardortiz84@gmail.com>
2026-05-30 23:34:28 -0700

    feat: add getPlaidItems() and unlinkItem() to AccountService

--- shared/src/commonMain/kotlin/com/daytimegaming/ourdailyfinances/data/network/service/AccountService.kt
+++ shared/src/commonMain/kotlin/com/daytimegaming/ourdailyfinances/data/network/service/AccountService.kt
@@ -5,7 +5,9 @@ import com.daytimegaming.ourdailyfinances.data.network.dto.AccountListResponse
 import com.daytimegaming.ourdailyfinances.data.network.dto.LinkExchangeRequest
 import com.daytimegaming.ourdailyfinances.data.network.dto.LinkExchangeResponse
 import com.daytimegaming.ourdailyfinances.data.network.dto.LinkTokenResponse
+import com.daytimegaming.ourdailyfinances.data.network.dto.PlaidItemsResponse
 import io.ktor.client.call.body
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
@@ -24,4 +26,11 @@ class AccountService(private val apiClient: ApiClient) {
             contentType(ContentType.Application.Json)
             setBody(LinkExchangeRequest(publicToken, institutionName))
         }.body()
+
+    suspend fun getPlaidItems(): PlaidItemsResponse =
+        apiClient.http.get("${apiClient.baseUrl}/plaid/items").body()
+
+    suspend fun unlinkItem(itemId: String) {
+        apiClient.http.delete("${apiClient.baseUrl}/plaid/items/$itemId")
+    }
 }
